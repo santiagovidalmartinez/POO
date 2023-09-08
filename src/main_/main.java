c5ee6eb0a28fb38f2cec777997998e2d691d8db8
santiagovidalmartinez <142599572+santiagovidalmartinez@users.noreply.github.com>
2023-09-08 08:31:03 -0500

    Add files via upload

---
+++ src/main_/main.java
@@ -0,0 +1,15 @@
+
+package main_;
+
+import poo.unicauca.edu.co.e1;
+
+public class main {
+
+    public static void main(String[] args) {
+        e1 call = new e1();
+        
+        System.out.println("First name: "+call.getFirstName() );
+        System.out.println("Last name: "+call.getLastName() );
+    }
+    
+}
---
+++ src/poo/unicauca/edu/co/e1.java
@@ -0,0 +1,26 @@
+
+package poo.unicauca.edu.co;
+
+public class e1 {
+    private String firstName = "Jhon";
+    private String lastName = "Edison";
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+
+}
+ 
